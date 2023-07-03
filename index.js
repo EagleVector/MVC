9b82268fcc1a1d6c842f33111800e23034a7aeab
EagleVector <kumarsubhambili3@gmail.com>
2023-07-03 09:59:25 +0530

    test

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 const PORT = 8000;
 
 // Connection 
-connectMongoDb("mongodb://127.0.0.1:27017/test-db")
+connectMongoDb("mongodb://127.0.0.1:27017/test-db").then(() => console.log("Mongodb Connected!"));
 
 // Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"))
 
 // Routes
-app.use("/user", userRouter);
+app.use("/api/users", userRouter);
 app.listen(PORT, console.log(`Server Started at PORT: ${PORT}`));
